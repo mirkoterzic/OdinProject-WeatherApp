9d9c709c0309ac5976265819e0a4dc2affa68c18
mirkoterzic <mirko.terzic2002@gmail.com>
2024-06-30 17:04:18 +0200

    added ability to change temp unit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const apiKey = "973f61eed37d43ae8fe90546242706";
+let currentUnit = "C"; // Default unit is Celsius
 
 document
   .getElementById("locationForm")
@@ -10,6 +11,22 @@ document
     await getWeather(location);
   });
 
+document.getElementById("toggleUnit").addEventListener("click", function () {
+  currentUnit = currentUnit === "C" ? "F" : "C";
+  const button = document.getElementById("toggleUnit");
+  button.innerText =
+    currentUnit === "C" ? "Switch to Fahrenheit" : "Switch to Celsius";
+  const weatherInfoDiv = document.getElementById("weatherInfo");
+  if (weatherInfoDiv.innerHTML) {
+    // If weather information is already displayed, refresh it with the new unit
+    const location = weatherInfoDiv.getAttribute("data-location");
+    const weatherData = JSON.parse(
+      weatherInfoDiv.getAttribute("data-weather-data")
+    );
+    displayWeather(weatherData);
+  }
+});
+
 async function getWeather(location) {
   try {
     const response = await fetch(
@@ -20,6 +37,12 @@ async function getWeather(location) {
     console.log(data);
     const weatherData = processWeatherData(data);
     displayWeather(weatherData);
+    document
+      .getElementById("weatherInfo")
+      .setAttribute("data-location", location);
+    document
+      .getElementById("weatherInfo")
+      .setAttribute("data-weather-data", JSON.stringify(weatherData));
   } catch (error) {
     document.getElementById("loading").style.display = "none";
     console.error("Error fetching weather data:", error);
@@ -33,7 +56,8 @@ function processWeatherData(data) {
     location: data.location.name,
     region: data.location.region,
     country: data.location.country,
-    temperature: data.current.temp_c,
+    temperatureC: data.current.temp_c,
+    temperatureF: data.current.temp_f,
     condition: data.current.condition.text,
     icon: data.current.condition.icon,
   };
@@ -41,10 +65,13 @@ function processWeatherData(data) {
 
 function displayWeather(weatherData) {
   const weatherInfoDiv = document.getElementById("weatherInfo");
+  const temperature =
+    currentUnit === "C" ? weatherData.temperatureC : weatherData.temperatureF;
+  const unit = currentUnit === "C" ? "°C" : "°F";
   weatherInfoDiv.innerHTML = `
         <h2>${weatherData.location}, ${weatherData.region}, ${weatherData.country}</h2>
         <img src="${weatherData.icon}" alt="${weatherData.condition}">
-        <p>Temperature: ${weatherData.temperature}°C</p>
+        <p>Temperature: ${temperature}${unit}</p>
         <p>Condition: ${weatherData.condition}</p>
     `;
 }
